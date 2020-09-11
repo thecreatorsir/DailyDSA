f755b1926cf52035acdd6973f574e39176c6a7ec
Shubham Sharma <s.psharma887@gmail.com>
2020-09-12 00:06:02 +0530

    day-36

---
+++ linkedLists/rotate-a-linked-list.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *rotate(struct Node *head, int k);
+
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, val, k;
+        cin>>n;
+        
+        cin>> val;
+        Node *head = new Node(val);
+        Node *tail = head;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            cin>> val;
+            tail->next = new Node(val);
+            tail = tail->next;
+        }
+        
+        cin>> k;
+        
+        head = rotate(head,k);
+        printList(head);
+    }
+    return 1;
+}
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// This function should rotate list counter-clockwise
+// by k and return new head (if changed)
+
+Node* rotate(Node* head, int k)
+{  
+    Node * p = head;
+    int count = 0;
+    while(p){
+        count++;
+        p=p->next;
+    }
+    if(k==count){
+        return head;
+    }
+    p=head;
+    while(p->next !=NULL){
+        p=p->next;
+    }
+    Node * tail = p;
+    p=head;
+    while(--k){
+        p=p->next;
+    }
+    Node * newhead = p->next;
+    p->next = NULL;
+    tail->next = head;
+    head = newhead;
+    return head;
+}
+
